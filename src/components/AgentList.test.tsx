19d2ed759b44820dd3aa1090fa1c5d2cdc981e96
jessicateh <jessicateh@hotmail.com>
2025-08-06 00:45:20 +1000

    Add tests

--- src/components/AgentList.test.tsx
+++ src/components/AgentList.test.tsx
@@ -1,9 +1,139 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen, within} from '@testing-library/react';
 import {AgentList} from "./AgentList";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
+import _ from "lodash";
 
-test('renders learn react link', () => {
-    render(<AgentList/>);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+// Cast `useQuery` to a mock function
+jest.mock('@tanstack/react-query', () => ({
+    ...jest.requireActual('@tanstack/react-query'),
+    useQuery: jest.fn(),
+}));
+const mockedUseQuery = useQuery as jest.Mock;
+
+const mockedAgents = [
+    {
+        "first_name": "Andy",
+        "last_name": "Dwyer",
+        "status": "offline",
+        "profile": "agent",
+        "avatar": "https://static1.personality-database.com/profile_images/639f9049c5554c6dbad3fc1a830d63c7.png"
+    },
+    {
+        "first_name": "April",
+        "last_name": "Ludgate",
+        "status": "online",
+        "profile": "agent",
+        "avatar": "https://static1.personality-database.com/profile_images/595d80646c4a4ce19f9637859e348111.png"
+    },
+];
+
+const queryClient = new QueryClient();
+
+function renderComponent() {
+    render(
+        <QueryClientProvider client={queryClient}>
+            <AgentList />
+        </QueryClientProvider>
+    );
+}
+
+describe('Agent List Component', () => {
+    test('renders agent list', () => {
+        mockedUseQuery.mockReturnValue({
+            data: [],
+            isLoading: false,
+            isError: false,
+            error: null,
+        });
+
+        renderComponent();
+
+        const title = screen.getByText('Agent List');
+        expect(title).toBeInTheDocument();
+
+        const listEl = screen.getByTestId('agent-list');
+        expect(listEl).toBeInTheDocument();
+
+    });
+
+    test('renders agents from API with all data points', () => {
+        // Use a mock agent returned as data by the API
+        mockedUseQuery.mockReturnValue({
+            data: [mockedAgents[0]],
+            isLoading: false,
+            isError: false,
+            error: null,
+        });
+
+        renderComponent();
+
+        const firstNameEl = screen.getByTestId('agent-first_name');
+        const lastNameEl = screen.getByTestId('agent-last_name');
+        const profileEl = screen.getByTestId('agent-profile');
+        const statusEl = screen.getByTestId('agent-status');
+        const avatarEl = screen.getByTestId('agent-avatar');
+
+        expect(firstNameEl).toBeInTheDocument();
+        expect(firstNameEl).toHaveTextContent(mockedAgents[0].first_name);
+        expect(lastNameEl).toBeInTheDocument();
+        expect(lastNameEl).toHaveTextContent(mockedAgents[0].last_name);
+        expect(statusEl).toBeInTheDocument();
+        expect(statusEl).toHaveTextContent(_.capitalize(mockedAgents[0].status));
+        // Check the Online status has the class to make the font color green
+        expect(within(statusEl).getByText('Online')).toHaveClass('status-online');
+
+        expect(profileEl).toBeInTheDocument();
+        expect(profileEl).toHaveTextContent(_.capitalize(mockedAgents[0].profile));
+        expect(avatarEl).toBeInTheDocument();
+        expect(avatarEl).toHaveAttribute('src', mockedAgents[0].avatar);
+        expect(avatarEl).toHaveAttribute('alt', `${mockedAgents[0].first_name} ${mockedAgents[0].last_name}`);
+    });
+
+    test('renders loading state', () => {
+        mockedUseQuery.mockReturnValue({
+            data: [],
+            isLoading: true,
+            isError: false,
+            error: null,
+        });
+
+        renderComponent();
+        expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
+
+    test('renders error state', () => {
+        mockedUseQuery.mockReturnValue({
+            data: [],
+            isLoading: false,
+            isError: true,
+            error: {message: '501 Service Error'},
+        });
+
+        renderComponent();
+        expect(screen.getByText('501 Service Error. Please try again later.')).toBeInTheDocument();
+    });
+
+    test('use can filter by Agent Status', () => {
+        mockedUseQuery.mockReturnValue({
+            data: mockedAgents,
+            isLoading: false,
+            isError: false,
+            error: null,
+        });
+
+        renderComponent();
+        // No filter so all agents displayed
+        expect(screen.getByText('April')).toBeInTheDocument();
+        expect(screen.getByText('Andy')).toBeInTheDocument();
+
+        const agentFilterSelect = screen.getByTestId('agent-filter');
+        expect(agentFilterSelect).toBeInTheDocument();
+        fireEvent.change(agentFilterSelect, { target: { value: 'online' } });
+
+        // Only April is online, so Andy should be filtered out
+        expect(screen.getByText('April')).toBeInTheDocument();
+        expect(screen.queryByText('Andy')).not.toBeInTheDocument();
+    });
 });
--- src/components/AgentList.tsx
+++ src/components/AgentList.tsx
@@ -2,21 +2,18 @@ import {AgentStatus, useAgents} from "../services/useAgents";
 import _ from "lodash";
 import {useEffect, useState} from "react";
 
-enum FilterOption {
-    NONE = 'None',
-    AGENT_STATUS = 'Agent Status'
-}
-
 export function AgentList() {
     const { agents, isLoading, isError, error} = useAgents();
-    const [selectedFilter, setSelectedFilter] = useState(FilterOption.NONE);
+    const filterOptions = ['All', AgentStatus.Online, AgentStatus.Offline]
+    const [selectedFilter, setSelectedFilter] = useState('All');
     const [filteredList, setFilteredList] = useState(agents);
 
+    // If the selected filter has changed, update the sorted list or return the original list
     useEffect(() => {
-        if (selectedFilter === FilterOption.AGENT_STATUS) {
-            setFilteredList(_.sortBy(agents, ['profile']))
-        } else {
+        if (selectedFilter === 'All') {
             setFilteredList(agents);
+        } else {
+            setFilteredList(agents?.filter(agent => agent.status === selectedFilter))
         }
     },[agents, selectedFilter]);
 
@@ -25,28 +22,39 @@ export function AgentList() {
             <h1>Agent List</h1>
             <div className="filter-container">
                 <form>
-                    <label htmlFor='filter'>Filter by</label>
-                    <select id='filter' value={selectedFilter} onChange={(e) => setSelectedFilter(e.target.value as FilterOption)}>
-                        {Object.values(FilterOption).map(filterOption =>
-                        <option key={filterOption}>{filterOption}</option>)}
+                    <label htmlFor='agent-filter'>Filter by</label>
+                    <select id='agent-filter'
+                            value={selectedFilter}
+                            onChange={(e) => setSelectedFilter(e.target.value)}
+                            data-testid='agent-filter'>
+                        {filterOptions.map(filterOption =>
+                        <option key={filterOption} value={filterOption}>{_.capitalize(filterOption)}</option>)}
                     </select>
                 </form>
             </div>
             <div>
                 {isLoading && <div>Loading...</div>}
-                {isError && <div>{error?.message}</div>}
+                {isError && <div>{error?.message}. Please try again later.</div>}
             </div>
-            <div className="card-list">
+            <div className="card-list" data-testid="agent-list">
                 {!isLoading && filteredList?.map(agent => {
-                    return <div className="agent-card" key={agent.first_name + agent.last_name}>
+                    return <div className="agent-card"
+                                key={agent.first_name + agent.last_name}
+                                data-testid='agent-card'>
                         <div className="avatar-wrapper">
-                            <img src={agent.avatar} alt="agent-avatar"/>
+                            <img src={agent.avatar}
+                                 alt={`${agent.first_name} ${agent.last_name}`}
+                                 data-testid='agent-avatar'/>
                         </div>
                         <div className="card-details">
-                            <div><b>First Name:</b> {agent.first_name}</div>
-                            <div><b>Last Name:</b> {agent.last_name}</div>
-                            <div><b>Agent Status:</b> {_.capitalize(agent.profile)}</div>
-                            <div><b>Status:</b> <span className={`status ${agent.status === AgentStatus.Online ? 'status-online' : ''}`}>{_.capitalize(agent.status)}</span></div>
+                            <div data-testid='agent-first_name'><b>First Name:</b> {agent.first_name}</div>
+                            <div data-testid='agent-last_name'><b>Last Name:</b> {agent.last_name}</div>
+                            <div data-testid='agent-profile'><b>Profile:</b> {_.capitalize(agent.profile)}</div>
+                            <div data-testid='agent-status'><b>Status:</b>
+                                <span className={`status ${agent.status === AgentStatus.Online ? 'status-online' : ''}`}>
+                                    {_.capitalize(agent.status)}
+                                </span>
+                            </div>
                         </div>
                     </div>
                 })}
